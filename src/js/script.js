2c080ad060eb6e84749ffc420aa814047ca0b353
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-08-07 16:28:24 +0200

    Generate HTML in class Product

--- src/js/script.js
+++ src/js/script.js
@@ -67,7 +67,8 @@
         const thisProduct = this;
 
         /* generate HTML based on template*/
-
+        const generatedHTML = templates.menuProduct(thisProduct.data);
+        //console.log(generatedHTML);
         /* create element using utils.createElementFromHTML */
 
         /* find menu container */
