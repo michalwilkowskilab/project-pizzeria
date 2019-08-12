d58c9ea016d522f825adfd85e6c92cc6bb1a5eee
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-08-12 13:03:15 +0200

    Add const formData = utils.serializeFormToOject()

--- src/js/script.js
+++ src/js/script.js
@@ -153,6 +153,9 @@
     processOrder(){
       const thisProduct = this;
       console.log(thisProduct);
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
     }
 
   }
